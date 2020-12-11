c09380095c1c6a488fc6448b35e7f78ae0486003
Virion <kajko195@tlen.pl>
2020-12-12 00:24:43 +0100

    complete reducers and build reducer composition
    complete handling add and remove actions and finish reducer to update
    state

--- client/redusers/comments.js
+++ client/redusers/comments.js
@@ -3,8 +3,36 @@
 // 1. the action (info about what happened)
 // 2. copy of current state
 
+function postComments(state = [], action) {
+    switch(action.type) {
+        case 'ADD_COMMENT':
+            //return the new state with new comment
+            return[...state, {
+                user: action.author,
+                text: action.comment
+            }];
+        case 'REMOVE_COMMENT':
+            //we need to return the new state without the deleted comment
+            return [
+                ...state.slice(0, action.i),
+                ...state.slice(action.i +1)
+            ]
+        default: 
+            return state
+    }
+        
+}
+
 function comments(state = [], action) {
-    console.log(state, action)
+    console.log(action.postId)
+    if(typeof action.postId !== 'undefined') {
+        return {
+            //take the current state
+            ...state,
+            //overwrite thispost with a new one
+            [action.postId]: postComments(state[action.postId], action)
+        }
+    }
     return state;
 }
 
